7daa47ee97ae19715cf171d15a76a74f28a3ba71
ColorMan777 <97909940+ColorMan777@users.noreply.github.com>
2026-01-11 00:27:23 +0100

    importUI update

--- Assets/Scripts/import_mode.gd
+++ Assets/Scripts/import_mode.gd
@@ -12,13 +12,19 @@ func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file(modeScene)
 
 func on_files_dropped(files):
+	import_files_list(files)
+	
+
+func import_files_list(files):
+	not_imported = [] # reset not imported (not cumulative like imported)
+	
 	for f in files:
 		if f.ends_with(".Map.Gbx"):
 			if import_files.find(f) == -1:
-				import_files.append(f)
+				import_files.append(f) #cumulative import
 		else:
 			if not_imported.find(f) == -1:
 				not_imported.append(f)
 	
-	print("imported : " + str(import_files))
-	print("not_imported : " + str(not_imported))
+	#print("imported : " + str(import_files))
+	#print("not_imported : " + str(not_imported))
